71874ecfaefe1948f2da0dedbfd35f52256a2897
MImranIqbal <se.imraniqbal@gmail.com>
2021-10-20 01:27:38 -0700

    src

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-let app = firebase.initializeApp({
+const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 
   authDomain: process.env.REACT_APP_FIREBASE_API_DOMAIN,
